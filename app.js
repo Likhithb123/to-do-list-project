967d028f8f781165d6aac32494ed6fe13b6b0a7d
Likhith Boola <137615143+Likhithb123@users.noreply.github.com>
2023-09-01 00:02:57 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+const taskForm = document.getElementById("task-form");
+const taskList = document.getElementById("task-list");
+ 
+//counter variable to track the task index
+
+let taskIndex = 1;;
+// console.log(taskForm,taskList);
+
+ taskForm.addEventListener("submit",function(event){
+    event.preventDefault();
+ const taskInput = document.getElementById("task-input");
+ const taskText = taskInput.value.trim();
+ // console.log(taskText);
+
+ if ( taskText !== ""){
+    // create a new task item
+   const taskItem = document.createElement("li");
+   taskItem.classList.add("task-item");
+   taskItem.textContent = `${taskIndex}- ${taskText}`;
+
+taskItem.addEventListener("click",function(){
+    this.classList.toggle("completed");
+});
+
+
+// Append the task item to the task list
+   taskList.appendChild(taskItem);
+   //Increment the task Index
+   taskIndex++;
+
+   taskInput.value = "";
+ }
+
+ });
